2036562ba581cb95cf42636093f4afdaf39d59a2
Kailash Patel <141627251+Kailash461@users.noreply.github.com>
2024-10-02 01:05:31 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 
 import { Navigate, Route, Routes } from 'react-router-dom';
-import './App.css';
+
 import Login from './pages/login';
 import Home from './pages/home';
 import Signup from './pages/signup';
